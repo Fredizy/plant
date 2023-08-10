66b5b57878c778209277227b568dc2b923d24eee
Asoata Fredrick <asoatafredrick1@gmail.com>
2023-08-10 12:40:52 -0700

    new file:   img/F2.jpg 	new file:   img/F3.jpg 	new file:   img/F4.jpg 	new file:   img/F5.jpg 	new file:   img/Img ads.jpg 	new file:   img/f1.jpg 	new file:   img/f6.jpg 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const menu = document.querySelector('.material-symbols-outlined');
+// const menu = document.querySelector('.material-symbols-outlined');
  
 
-menu.addEventListener('click', () => {
-   sidebar.style.opacity = 100;
+// menu.addEventListener('click', () => {
+//    sidebar.style.opacity = 100;
+// });
+
+// Search
+const searchInput = document.querySelector('input[type="search"]');
+const boxes = document.querySelectorAll('.box');
+
+searchInput.addEventListener('input', () => {
+  const searchTerm = searchInput.value.toLowerCase().trim();
+  
+  boxes.forEach(box => {
+    const title = box.querySelector('.boxed-bottom-text p').textContent.toLowerCase();
+    
+    if (title.includes(searchTerm)) {
+      box.style.display = 'block';
+    } else {
+      box.style.display = 'none';
+      
+    }
+  });
 });
